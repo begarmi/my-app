b2d835ad5ed3b13e23972c26b70511303e25893c
Armin Beganovic <beganovic.armi@gmail.com>
2025-05-06 12:07:39 +0200

    close sidebar on click outside

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,16 +1,31 @@
 <template>
   <nav class="menu" ref="menuRef">
-    <button @click="$emit('toggle-sidebar')" class="menu-button">
+    <button @click="toggleSidebar" class="menu-button">
       Open Sidebar
     </button>
     <NuxtLink to="/" class="menu-link">Home</NuxtLink>
+
+    <Sidebar :visible="showSidebar" @close="showSidebar = false">
+      <Categories />
+    </Sidebar>
   </nav>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import { useClickOutside } from '~/composables/useClickOutside'
+import Sidebar from './Sidebar.vue'
 
-defineEmits(['toggle-sidebar'])
-
+const showSidebar = ref(false)
 const menuRef = ref(null)
+
+const toggleSidebar = () => {
+  showSidebar.value = !showSidebar.value
+}
+
+useClickOutside(menuRef, () => {
+  if (showSidebar.value) {
+    showSidebar.value = false
+  }
+})
 </script>
