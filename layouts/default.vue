cf84cc89700279cf01ae163e185edf23403cc47d
Armin Beganovic <beganovic.armi@gmail.com>
2025-05-07 11:57:25 +0200

    remove old code

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,10 @@
 <template>
     <div>
-      <Menu @toggle-sidebar="isSidebarVisible = true" />
-      <Sidebar :visible="isSidebarVisible" @close="isSidebarVisible = false">
-        <Categories />
-      </Sidebar>
+      <Menu />
       <NuxtPage />
     </div>
   </template>
   
   <script setup>
-  import { ref } from 'vue'
-  
-  const isSidebarVisible = ref(false)
   </script>
   
